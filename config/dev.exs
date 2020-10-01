55891d1f6a399aa207735c86f3ebdacc6c4ce30e
Omori Yasumitsu <yasumitsuoomori@gmail.com>
2020-10-01 17:01:32 +0900

    fix: Disable codereloader to release

--- config/dev.exs
+++ config/dev.exs
@@ -9,7 +9,7 @@ use Mix.Config
 config :websocket, WebsocketWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
-  code_reloader: true,
+  code_reloader: false,
   check_origin: false,
   watchers: [
     node: [
